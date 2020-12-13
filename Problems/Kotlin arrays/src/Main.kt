91b862e6e24c1ccd99193c97ceea2d12015dd5e3
Nicolas Mahnic <nico.mahnic94@gmail.com>
2020-12-13 19:19:03 -0300

    multiarray

---
+++ Problems/Kotlin arrays/src/Main.kt
@@ -0,0 +1,7 @@
+/*
+    for(i in 0..inputArray[2].size-2) {
+        print("${inputArray[2][i]}, ")
+    }
+    print("${inputArray[2][inputArray[2].size-1]}")
+*/
+println(inputArray[2].joinToString())
