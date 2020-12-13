e56a595a2fdfdac3dc3ad45eecc828ff6594d27f
Nicolas Mahnic <nico.mahnic94@gmail.com>
2020-12-13 19:27:24 -0300

    nestedArray

---
+++ Problems/Cube of zeros/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //TODO()
+
+    val matrix = arrayOf(
+            arrayOf(arrayOf(0, 0, 0), arrayOf(0, 0, 0), arrayOf(0, 0, 0)),
+            arrayOf(arrayOf(0, 0, 0), arrayOf(0, 0, 0), arrayOf(0, 0, 0)),
+            arrayOf(arrayOf(0, 0, 0), arrayOf(0, 0, 0), arrayOf(0, 0, 0))
+    )
+    println(matrix.contentDeepToString())
+}
