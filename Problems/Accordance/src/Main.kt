2f52563b459c0d94ce9a8449b362a76abda35186
Nicolas Mahnic <nico.mahnic94@gmail.com>
2020-12-25 04:53:32 -0300

    add last problem

---
+++ Problems/Accordance/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // put your code here
+    val inputArray = arrayOf(
+                            arrayOf("[0][0]","[0][1]","[0][2]"),
+                            arrayOf("[1][0]","[1][1]","[1][2]")
+    )
+
+
+    //println(inputArray[1][0])
+    println(inputArray.contentDeepToString())
+}
